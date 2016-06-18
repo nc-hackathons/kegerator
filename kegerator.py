4edfb4f35dd3323870fb583628f735dd4b53d7d2
Nikhil Wahi <nikhilwahi1995@gmail.com>
2016-06-18 16:03:46 -0500

    diff

--- kegerator.py
+++ kegerator.py
@@ -33,9 +33,10 @@ GPIO.add_event_detect(KEG_PIN_1, GPIO.RISING, callback=doAClick, bouncetime=20) 
 # main loop
 while True:
   currentTime = int(time.time() * 1000)
-  if (fm.thisPour > 0.23 and currentTime - fm.lastClick > 10000): # 10 seconds of inactivity causes a tweet
+  print fm.lastClick
+  if (currentTime - fm.lastClick > 10000): # 10 seconds of inactivity causes a tweet
     print "Someone just poured " + fm.getFormattedThisPour() + " of beer from the keg"
 
   # reset flow meter after each pour (2 secs of inactivity)
-  if (fm.thisPour <= 0.23 and currentTime - fm.lastClick > 2000):
+  if (currentTime - fm.lastClick > 2000):
     fm.thisPour = 0.0
