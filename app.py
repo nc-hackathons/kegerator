9b627261460efa27cd948aa24e7a6021d6e4f6a5
Sameet Sapra <sameetandpotatoes@gmail.com>
2016-06-18 17:53:52 -0500

    Add database credentials

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
-from flask import Flask
-import api
-import IPython
+#!/usr/bin/python
 import os
 import time
 import math
 import logging
-import RPi.GPIO as GPIO
 from Flow_Meter import *
-app = Flask(__name__)
-
-@app.route('/list')
-def list_current_batches():
-    return api.list_current_batches()
-
-app.run()
-
+import RPi.GPIO as GPIO
 
 KEG_PIN_1 = 4
+
 GPIO.setmode(GPIO.BCM) # use real GPIO numbering
 GPIO.setup(KEG_PIN_1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 fm = Flow_Meter(1)
+
+# Beer, on Pin 4
 def doAClick(channel):
   currentTime = int(time.time() * 1000)
   if fm.enabled == True:
@@ -30,8 +25,17 @@ def sendData(flow_meter):
   flow_meter.getFormattedThisPour()
   flow_meter.fm_id
 
-GPIO.add_event_detect(KEG_PIN_1, GPIO.RISING, callback=doAClick, bouncetime=20) # Beer, on Pin 23
+"""
+def doAClick2(channel):
+  currentTime = int(time.time() * FlowMeter.MS_IN_A_SECOND)
+  if fm2.enabled == True:
+    fm2.update(currentTime)
+"""
 
+GPIO.add_event_detect(KEG_PIN_1, GPIO.RISING, callback=doAClick, bouncetime=20) # Beer, on Pin 23
+#GPIO.add_event_detect(24, GPIO.RISING, callback=doAClick2, bouncetime=20) # Root Beer, on Pin 24
+
+# main loop
 while True:
   currentTime = int(time.time() * 1000)
   print fm.lastClick
