e91249bffd5afb392118800e9d573a066bc6c43c
NextCapital Kegerator <keg@nextcapital.com>
2016-06-22 01:36:18 +0000

    Swap SQS for web sockets

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import time
 import math
 import logging
 import json
-import boto3
 from Flow_Meter import *
 import RPi.GPIO as GPIO
 from models import *
 from sqlalchemy import *
+
+import httplib
 
 KEG_PIN_1 = 4
 KEG_PIN_2 = 21
@@ -16,9 +17,6 @@ KEG_PIN_2 = 21
 GPIO.setmode(GPIO.BCM) # use real GPIO numbering
 GPIO.setup(KEG_PIN_1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(KEG_PIN_2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-sqs = boto3.resource('sqs', region_name='us-east-1')
-queue = sqs.get_queue_by_name(QueueName='keg-o-meter')
 
 fm1 = Flow_Meter(1)
 fm2 = Flow_Meter(2)
@@ -29,8 +27,11 @@ def doAClick(channel):
   if fm1.enabled == True:
     if fm1.clicks >= 2 and fm1.start_pouring == True:
         fm1.start_pouring = False
-    	send_sqs_message("ON")
         print "Someone just started pouring!"
+	conn = httplib.HTTPConnection("localhost", 5000)
+	fm1_id = fm1.fm_id
+	headers = {"Content-type": "application/json"}
+	conn.request("POST", "/pour_event", "{ \"keg\": %(fm1_id)s, \"tap_position\": \"ON\" }" % locals(), headers)
     fm1.update(currentTime)
 
 
@@ -39,18 +40,12 @@ def doAClick2(channel):
   if fm2.enabled == True:
     if fm2.clicks >= 2 and fm2.start_pouring == True:
         fm2.start_pouring = False
-        send_sqs_message("ON")
         print "Someone just started pouring!"
+	conn = httplib.HTTPConnection("localhost", 5000)
+	fm2_id = fm2.fm_id
+	headers = {"Content-type": "application/json"}
+	conn.request("POST", "/pour_event", "{ \"keg\": %(fm2_id)s, \"tap_position\": \"ON\" }" % locals(), headers)
     fm2.update(currentTime)
-
-
-def send_sqs_message(subject, amount=None):
-  message = {
-	'subject': subject,
-  }
-  if amount is not None:
-    message['beer'] = amount
-  queue.send_message(MessageBody=json.dumps(message))
 
 
 def sendData(flow_meter):
@@ -60,7 +55,9 @@ def sendData(flow_meter):
   pour_activity = Pour(current_batch, amount_poured)
   db.session.add(pour_activity)
   db.session.commit() # Adds pour to database
-  send_sqs_message("OFF", amount_poured)
+  conn = httplib.HTTPConnection("localhost", 5000)
+  headers = {"Content-type": "application/json"}
+  conn.request("POST", "/pour_event", "{ \"keg\": %(keg_id)s, \"tap_position\": \"OFF\" }" % locals(), headers)
 
 
 GPIO.add_event_detect(KEG_PIN_1, GPIO.RISING, callback=doAClick, bouncetime=10) # Beer, on Pin 23
